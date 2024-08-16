9c63bf3d951c0f0a6260efa210776fcc75e4b4e7
Roberto Nares <roberto.nares@gmail.com>
2024-08-16 13:46:23 -0700

    Perform integration test for current functionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,30 @@
 # frozen_string_literal: true
 
+require 'colorize'
 require_relative 'game_io'
+require_relative 'board'
 
 # Class for connect four gameplay
 class Game
   include GameIO
 
+  PLAYER_TOKEN_1 = "\u25CF".encode('UTF-8').colorize(color: :blue)
+  PLAYER_TOKEN_2 = "\u25CF".encode('UTF-8').colorize(color: :red)
+
   def initialize(board = Board.new)
     @board = board
+  end
+
+  def play
+    board.insert(0, PLAYER_TOKEN_1)
+    board.insert(0, PLAYER_TOKEN_2)
+    board.insert(1, PLAYER_TOKEN_2)
+    board.insert(1, PLAYER_TOKEN_1)
+    board.insert(2, PLAYER_TOKEN_1)
+    board.insert(2, PLAYER_TOKEN_1)
+    board.insert(6, PLAYER_TOKEN_2)
+    board.insert(6, PLAYER_TOKEN_2)
+    next_column_choice
   end
 
   private
